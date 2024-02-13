f9117149a5cc2e850a7505969e442d4faf0af80e
PijusPale <90030227+PijusPale@users.noreply.github.com>
2024-02-13 22:20:28 +0200

    add queries to repos

--- src/main/java/com/pk/messenger/repository/MessageRepository.java
+++ src/main/java/com/pk/messenger/repository/MessageRepository.java
@@ -2,6 +2,15 @@ package com.pk.messenger.repository;
 
 import com.pk.messenger.model.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
+    @Query(nativeQuery = true, value = "SELECT m.* " +
+            "FROM my_User mu " +
+            "JOIN message m ON mu.id = m.user_Id " +
+            "WHERE mu.id = :userId")
+    List<Message> findAllByUserId(@Param("userId") Long userId);
 }
--- src/main/java/com/pk/messenger/repository/MyUserRepository.java
+++ src/main/java/com/pk/messenger/repository/MyUserRepository.java
@@ -1,8 +1,12 @@
 package com.pk.messenger.repository;
 
+import com.pk.messenger.model.Message;
 import com.pk.messenger.model.MyUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MyUserRepository extends JpaRepository<MyUser, Long> {
-
+    @Query(nativeQuery = true, value = "SELECT * FROM my_User WHERE username = :username")
+    MyUser findByUsername(@Param("username") String username);
 }
